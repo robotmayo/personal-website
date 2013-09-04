4ac88005847a90b9aba9fbff64efcd2c40979c97
Nigel-Edwards <robotmayo@gmail.com>
2013-09-04 17:59:34 -0400

    I Wont forget next time..

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
-	var INT_TOP_ZINDEX = 1000;
-	var INT_HIDDEN = 99;
+	var zindexes = [1000,900,800];
+	var zIndexList ={top:1000,behind:900,bottom:800};
+	var sections = [];
+	var activeSection;
+	var lock = false;
+	var selection;
+	var scrolled = false;
+
+	$("section").each(function(){ sections.push($(this))});
+	for(var i= sections.length-1; i >= 1; i--){
+		sections[i].hide();
+	}
+	activeSection = sections[0];
+	activeSection.css('z-index',zindexes[0]);
+
 	$('#nav').children('a').click(function(e){
 		e.preventDefault();
-		var self = $(this);
-		if(self.css("z-index") == hidden+""){
-			alert("POTATO");
+		if(lock) return;
+		selection = $($(this).attr('href'));
+		if(selection[0] == activeSection[0]){
+			$('html,body').animate({scrollTop:0}, 400);
+			return;
 		}
-		var siteSection = $(this).attr('href');
-		console.log(siteSection);
-		//$(siteSection).css('z-index',INT_TOP_ZINDEX);
-		$('body').css('overflow-y', 'hidden');
-		var scrollBottom = $(window).scrollTop() + $(window).height();
-		$(siteSection).animate({'top': scrollBottom},1000,"easeInExpo");
+		lock = true;
+		if($(window).scrollTop() > 20){
+			$('html,body').animate({scrollTop:0}, 400, scrollComplete);
+			//$('body').animate({scrollTop:0}, 400, scrollComplete);
+		}else{
+			animatePage();
+		}
+		
 	});
+	function animatePage(){
+		selection.css('z-index',zindexes[1]);
+		var scrollBottom = $(window).scrollTop() + $(window).height();
+		selection.show();
+		activeSection.animate({'left': -activeSection.width()},800,"easeInExpo",function(){
+			$(this).css('z-index',zindexes[1]);
+			activeSection.css('z-index',zindexes[0]);
+			$(this).css('left',0);
+			lock = false;
+		});
+		activeSection = selection;
+	}
+	function scrollComplete(){
+		if(scrolled) {
+			scrolled = false;
+			return;
+		}
+		scrolled = true;
+		animatePage();
+	}
 });
